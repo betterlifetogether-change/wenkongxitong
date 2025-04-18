fdbeaf8a47237678c3963270258443ef52bc707f
betterlifetogether-change <1786954443@qq.com>
2025-04-18 17:29:56 +0800

    4_18_1

--- core_algorithm_layer.py
+++ core_algorithm_layer.py
@@ -1,30 +1,34 @@
 import math
 
 class ThermodynamicsModel:
-    def calculate_flow_ratio(self,T_hot,T_cold,T_target,Q_tatal):
-        rho_hot=1000-0.0178*pow(T_hot,1.5)
-        rho_cold=1000-0.0178*pow(T_cold,1.5)
-        Qh=(T_target-T_cold)/(T_hot-T_cold)
-        Qc=1-Qh
-
-        k=math.sqrt(rho_cold/rho_hot)
-        hot_ratio=Qh/(Qh+Qc*k)
-        cold_ratio=1-hot_ratio
+    def calculate_flow_ratio(self,T_hot,T_cold,T_target,Q_total):
+        delta_total = T_hot - T_cold
+        delta_cold = T_target-T_cold
+        delta_hot = T_hot-T_target
+        hot_ratio = Q_total*delta_cold/delta_total
+        cold_ratio = Q_total*delta_hot/delta_total
         return hot_ratio,cold_ratio
 
 class ValveOpening:
     def water_volume(self,Q_total,T_hot,T_cold,T_target):
         TM=ThermodynamicsModel
-        hot_ratio,cold_ratio=TM.calculate_flow_ratio(T_hot,T_cold,T_target,Q_total)
-        Q_hot=Q_total*hot_ratio
-        Q_cold=Q_total*cold_ratio
+        Q_hot,Q_cold=TM.calculate_flow_ratio(T_hot,T_cold,T_target,Q_total)
         return Q_hot,Q_cold
 
     def pressure_diff_calculation(self,P_hot_initial,P_cold_initial,P_end):
+        P_hot=P_hot_initial-P_end
+        P_cold=P_cold_initial-P_end
+        return P_hot,P_cold
 
     def diff_temp_rou(self,T_hot,T_cold):
+        # 后期需要调用freesteam库进行精确计算
+        # 常压下
+        rou_hot=((999.8396+16.945176*T_hot)-(7.9870401*0.001*pow(T_hot,2)))/(1+16.879850*0.001*T_hot)
+        rou_cold = ((999.8396 + 16.945176 * T_cold) - (7.9870401 * 0.001 * pow(T_cold, 2))) / (
+                    1 + 16.879850 * 0.001 * T_cold)
+        return rou_hot,rou_cold
 
-    def cross_sectional_area_percentage(self,Q_hot,Q_cold,K_hot,P_hot,rou_hot,K_cold,P_cold,rou_cold):
+    def cross_sectional_area_percentage(self,P_hot,P_cold,K_hot,rou_hot,Q_hot,K_cold,rou_cold,Q_cold):
         A_hot=Q_hot/(K_hot*math.sqrt(2*P_hot/rou_hot))
         A_cold=Q_cold/(K_cold*math.sqrt(2*P_cold/rou_cold))
         return A_hot,A_cold
